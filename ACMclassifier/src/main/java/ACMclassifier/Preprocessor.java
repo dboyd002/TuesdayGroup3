acad55791e18d51b955c939f994b6529e49a7d02
dboyd002 <dboyd002@odu.edu>
2022-11-14 09:53:51 -0500

    Added Preprocessor class to the project.

---
+++ ACMclassifier/src/main/java/ACMclassifier/Preprocessor.java
@@ -0,0 +1,38 @@
+package ACMclassifier;
+
+public class Preprocessor {
+	
+	//Accessors:
+	
+	//Stored in class Document
+	//getRawSignature()
+	
+	//Stored in class Document
+	//getTokens()
+	
+	//Stored in class Interface
+	//getStopList()
+	
+	//Mutators:
+	
+	public void normalizeRawSignature() {
+		
+		
+		
+	}
+	
+	//Use tokens from Document class to count the word frequency
+	public int countWordFrequency() {
+		
+		return 0;
+		
+	}
+	
+	//Prune undesired words from the signature using the stop list (stored as a set in class Interface)
+	public void removeStopWords() {
+		
+		
+		
+	}
+	
+}
