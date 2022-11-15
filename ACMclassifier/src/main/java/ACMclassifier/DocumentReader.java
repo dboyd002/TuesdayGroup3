b14a276489c973d491d3c4f4f7bf9cbe48407e19
lvabby <45014221+lvabby@users.noreply.github.com>
2022-11-15 15:21:07 -0500

    Update DocumentReader.java

--- ACMclassifier/src/main/java/ACMclassifier/DocumentReader.java
+++ ACMclassifier/src/main/java/ACMclassifier/DocumentReader.java
@@ -5,13 +5,7 @@ import java.util.Scanner;
 
 
 public class DocumentReader {
-	public static void main(String[] args) {
-		
-		Scanner read = new Scanner(System.in);
-		System.out.println("Enter file path: ");
-		String filePath = read.nextLine();
-		parseToPlainText(filePath);
-	}
+
 	
 public String parseToPlainText(String filePath) throws IOException, SAXException, TikaException {
 	BodyContentHandler handler = new BodyContentHandler();
