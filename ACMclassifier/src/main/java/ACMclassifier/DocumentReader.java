c358ccf8963c0acc422c0dbaff67936d005fa8b3
lvabby <45014221+lvabby@users.noreply.github.com>
2022-11-17 15:32:01 -0500

    Update DocumentReader.java

--- ACMclassifier/src/main/java/ACMclassifier/DocumentReader.java
+++ ACMclassifier/src/main/java/ACMclassifier/DocumentReader.java
@@ -1,20 +1,45 @@
 package ACMclassifier;
+package org.ea;
+
+import org.apache.tika.Tika;
+import org.apache.tika.extractor.EmbeddedDocumentExtractor;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BasicContentHandlerFactory;
+import org.xml.sax.ContentHandler;
+
 import java.io.*;
-import org.apache.tika.*;
-import java.util.Scanner;
 
 
-public class DocumentReader {
-
+public class DocumentReader{
 	
-public String parseToPlainText(String filePath) throws IOException, SAXException, TikaException {
-	BodyContentHandler handler = new BodyContentHandler();
- 	AutoDetectParser parser = new AutoDetectParser();
-    	Metadata metadata = new Metadata();
-   	try (InputStream stream = ContentHandlerExample.class.getResourceAsStream(filePath)) {
-        	parser.parse(stream, handler, metadata);
-        	return handler.toString();
-    	}
-}
+    public static String parseDoc(String filename) {
+        try {
+            BasicContentHandlerFactory basicHandlerFactory = new BasicContentHandlerFactory(
+                    BasicContentHandlerFactory.HANDLER_TYPE.XML, -1
+            );
+            AutoDetectParser parser = new AutoDetectParser();
+            ContentHandler handler = basicHandlerFactory.getNewContentHandler();
+            Metadata metadata = new Metadata();
+
+            Tika tika = new Tika();
+            ParseContext context = new ParseContext();
+            context.set(
+                EmbeddedDocumentExtractor.class,
+                new FileEmbeddedDocumentExtractor(tika.getDetector())
+            );
+            context.set(Parser.class, parser);
+
+            File docFile = new File(filename);
+            InputStream stream = new FileInputStream(docFile);
+            parser.parse(stream, handler, metadata, context);
+
+            return handler.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+      }// end parseDoc
+        return null;
 
 }// end class
