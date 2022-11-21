6c3219199e3d392aef4c84dabf73b65f0dca8021
dboyd002 <dboyd002@odu.edu>
2022-11-21 11:08:38 -0500

    Created Driver class with main function.
    Added Far Jar creation code in build.gradle.
    
    Moved Document.java to ACMclassifier package.

---
+++ ACMclassifier/src/main/java/ACMclassifier/Driver.java
@@ -0,0 +1,23 @@
+package ACMclassifier;
+
+import java.util.Scanner;
+
+public class Driver {
+	
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter a file path:");
+		
+		String filePath = in.nextLine();
+		
+		in.close();
+		
+		System.out.println(filePath);
+		
+		DocumentReader.parseDoc(filePath);
+		
+	}
+
+}
