d92e60dc7e3e648acf0da494d3b9e94b257a5801
PRINCE <oosho001@odu.edu>
2022-11-21 22:06:10 +0300

    Driver class

--- ACMclassifier/src/main/java/ACMclassifier/Driver.java
+++ ACMclassifier/src/main/java/ACMclassifier/Driver.java
@@ -1,7 +1,19 @@
-//prince
-
 package ACMclassifier;
 
-public class Driver {
+import java.io.*;
+import org.apache.tika.*;
+import java.util.Scanner;
+
+public class ACMclassifierDriver {
+
+    public String parseToPlainText(String filePath) throws IOException, SAXException, TikaException {
+        BodyContentHandler handler = new BodyContentHandler();
+        AutoDetectParser parser = new AutoDetectParser();
+        Metadata metadata = new Metadata();
+        try (InputStream stream = ContentHandlerExample.class.getResourceAsStream(filePath)) {
+            parser.parse(stream, handler, metadata);
+            return handler.toString();
+        }
+    }
 
 }
