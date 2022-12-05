9d4153dc3122954b36b8bc1604b04f26820d8c01
dboyd002 <dboyd002@odu.edu>
2022-12-04 21:34:35 -0500

    Fixed Driver class - takes file path from the user and passes it to the document parser.

--- ACMclassifier/src/main/java/ACMclassifier/Driver.java
+++ ACMclassifier/src/main/java/ACMclassifier/Driver.java
@@ -1,21 +1,23 @@
-//prince
-
 package ACMclassifier;
 
-import java.io.*;
-import org.apache.tika.*;
 import java.util.Scanner;
 
-public class ACMclassifierDriver {
+public class Driver {
 
-    public String parseToPlainText(String filePath) throws IOException, SAXException, TikaException {
-        BodyContentHandler handler = new BodyContentHandler();
-        AutoDetectParser parser = new AutoDetectParser();
-        Metadata metadata = new Metadata();
-        try (InputStream stream = ContentHandlerExample.class.getResourceAsStream(filePath)) {
-            parser.parse(stream, handler, metadata);
-            return handler.toString();
-        }
-    }
+	public static void main(String args[]) {
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Please enter a file path:");
+
+		String filePath = in.nextLine();
+
+		in.close();
+
+		System.out.println(filePath);
+
+		DocumentReader.docParser(filePath);
+
+	}
 
 }
